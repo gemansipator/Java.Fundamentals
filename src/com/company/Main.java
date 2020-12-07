a81b5685d152eab365f5ea223589de973c667761
Nikolay Lenovo Home <gemansipator@gmail.com>
2020-12-07 18:54:46 +0400

    1. Поприветствовать любого пользователя при вводе его имени через командную строку

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner youName = new Scanner(System.in);
+        System.out.println("Введите имя:  ");
+        String name = youName.nextLine();
+        System.out.print("Привет, " + name +"! Как дела?");
     }
 }
